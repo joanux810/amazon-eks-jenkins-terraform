0adefb38ea06c4ba8f9359489a3aa4190cad143d
root <root@ip-172-31-27-215.us-east-2.compute.internal>
2022-04-01 04:29:23 +0000

    updated the jenkins repo, the keypair name and the region that corresponds to the keypair.

--- terraform/install_jenkins.sh
+++ terraform/install_jenkins.sh
@@ -21,11 +21,12 @@ yum install docker -y
 sudo chkconfig docker on
 
 echo "Install Jenkins"
-wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
 yum install -y jenkins
 sudo usermod -a -G docker jenkins
 sudo chkconfig jenkins on
 sudo service docker start
 sudo service jenkins start
-
